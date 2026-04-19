0fdd9c640f240082d6a8d6b0fe0c98ceef62fff6
Afnan Khan <afnankhanwork@gmail.com>
2026-04-19 05:02:24 -0700

    Update firebase config for Render

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,23 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
-import os
-
-# Path to your service account JSON
-SERVICE_ACCOUNT_PATH = os.path.join(os.path.dirname(__file__), "serviceAccountKey.json")
 
 # Initialize Firebase only once
 if not firebase_admin._apps:
-    cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
-    firebase_admin.initialize_app(cred)
+    firebase_key = os.getenv("FIREBASE_KEY")
 
-# Firestore DB instance
+    if not firebase_key:
+        raise Exception("FIREBASE_KEY environment variable is missing")
+
+    try:
+        cred_dict = json.loads(firebase_key)
+        cred = credentials.Certificate(cred_dict)
+        firebase_admin.initialize_app(cred)
+        print("Firebase initialized successfully")
+
+    except Exception as e:
+        raise Exception(f"Firebase initialization failed: {str(e)}")
+
+# Firestore database instance
 db = firestore.client()
